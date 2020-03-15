84b7ea1d1018f01ab1fea692a91ec99d6bc8c87d
rigondo <chan7749@hanmail.net>
2020-03-15 23:18:14 +0900

    home add glide.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
-const test = () => {
-  console.log('hi');
-};
+import Glide from "@glidejs/glide";
+
+new Glide(".glide", {
+	type: "carousel1",
+	startAt: 0,
+	perView: 1,
+	autoplay: 3500,
+	animationDuration: 100
+}).mount();
